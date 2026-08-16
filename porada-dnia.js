2f2c7c0f7867d36fd1e81ff105f744906ddf979e
wmkraw-web <wmkraw@gmail.com>
2026-08-16 16:27:43 +0200

    Create porada-dnia.js

---
+++ porada-dnia.js
@@ -0,0 +1,4 @@
+window.PORADA_DNIA = {
+  "text": "Zacznij lekcję od jednego pytania otwierającego zamiast listy obecności – uczniowie szybciej się skupiają, a Ty zyskujesz naturalne wprowadzenie do tematu.",
+  "date": "2026-08-16"
+};
