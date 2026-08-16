fd2c26aedf47f4de9d745d44c6a3557539eeb87f
wmkraw-web <wmkraw@gmail.com>
2026-08-16 18:00:29 +0200

    Add files via upload

---
+++ ogloszenie.js
@@ -0,0 +1,8 @@
+window.OGLOSZENIE = {
+  "id": "2026-08-16-start",
+  "active": false,
+  "title": "Nowość w EduBox AI!",
+  "message": "Tutaj wpisz treść ogłoszenia - np. o nowym narzędziu albo nowej funkcji. Gdy chcesz je pokazać odwiedzającym, zmień \"active\" na true i zapisz plik.",
+  "linkText": "Zobacz",
+  "linkUrl": "index.html"
+};
