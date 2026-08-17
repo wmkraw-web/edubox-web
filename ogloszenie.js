fba84d853c8601ca7d36422c4a16d91f4c2b3998
wmkraw-web <wmkraw@gmail.com>
2026-08-17 10:32:56 +0200

    Aktualizacja ogłoszenia (Redaktor Ogłoszeń)

--- ogloszenie.js
+++ ogloszenie.js
@@ -1,5 +1,5 @@
 window.OGLOSZENIE = {
-  "id": "2026-08-16T19:16:16.691Z",
+  "id": "2026-08-17T08:32:54.351Z",
   "active": true,
   "title": "Nowość w EduBox AI!",
   "message": "Dodałem właśnie małą aplikację EduDyplom Video. Możecie teraz tworzyć atrakcyjne dyplomy wideo i wysyłać rodzicom .",
