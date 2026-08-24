fcb092e3cddc8606f2a9a71eec860d4ddae31cc0
wmkraw-web <wmkraw@gmail.com>
2026-08-25 00:06:29 +0200

    Aktualizacja ogłoszenia (Redaktor Ogłoszeń)

--- ogloszenie.js
+++ ogloszenie.js
@@ -1,5 +1,5 @@
 window.OGLOSZENIE = {
-  "id": "2026-08-24T21:49:27.351Z",
+  "id": "2026-08-24T22:06:27.718Z",
   "active": true,
   "title": "Nowość w EduBox AI!",
   "message": "Dzisiaj dodałem aplikację EduLekcja 360. To takie CENTRUM DOWODZENIA LEKCJĄ - zaplanuje kompletną lekcję: cele, przebieg, zadania, quiz, kartę wyjścia i dostosowania SPE.",
